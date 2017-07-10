d2f8e1023e2af21803a14b642bb55f5fc6bc1f58
mtadayz <rysttimes@gmail.com>
2017-07-10 18:11:45 +0200

    Basic stuff

---
+++ battlegrounds/hud/hud_c.lua
@@ -0,0 +1,12 @@
+
+local zoneBlip = false
+function createCustomBlip(zone,radius)
+	if zoneBlip then
+		exports.customblips:destroyCustomBlip(zoneBlip)
+	end
+	local x,y,z = getElementPosition(zone)
+	zoneBlip = exports.customblips:createCustomBlip(x,y,radius/4,radius/4,"hud/radius.png",radius)
+	exports.customblips:setCustomBlipRadarScale(zoneBlip,1)
+end
+addEvent("createCustomBlip",true)
+addEventHandler("createCustomBlip",root,createCustomBlip)
---
+++ battlegrounds/hud/hud_s.lua
@@ -0,0 +1,28 @@
+
+
+local theZone
+local zoneRadius = 3000
+local radiusTimer = 300000
+function createZone(plr)
+	local x,y = math.random(-2500,2500), math.random(-2500,2500)
+	theZone = createColCircle(x,y,zoneRadius)
+	local x,y,z = getElementPosition(theZone)
+	outputServerLog("Zone has been created: "..tostring(x)..", "..tostring(y))
+	setTimer(decreaseZoneSize,radiusTimer,0)
+	triggerClientEvent("createCustomBlip",root,theZone,zoneRadius) 
+end
+addCommandHandler("zone",createZone)
+
+function decreaseZoneSize()
+	if zoneRadius > 39.75 then
+		local oldX,oldY,oldZ = getElementPosition(theZone)
+		destroyElement(theZone)
+		zoneRadius = zoneRadius-(zoneRadius*0.25)
+		if radiusTimer > 60000 then
+			radiusTimer = radiusTimer-60000
+		end
+		theZone = createColSphere(oldX,oldY,oldZ,zoneRadius)
+		outputServerLog("Zonesize has been decreased!")
+		triggerClientEvent("createCustomBlip",root,theZone,zoneRadius)
+	end
+end
