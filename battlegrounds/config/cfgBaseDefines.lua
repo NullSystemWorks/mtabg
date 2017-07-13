3302b8ca3498f7453759d861641eaab7ff5d02df
ciber96 <keevgross@gmail.com>
2017-07-13 15:47:25 -0300

    Added baseConfig

---
+++ battlegrounds/config/cfgBaseDefines.lua
@@ -0,0 +1,12 @@
+--[[
+	
+				MTA:BG
+			MTA Battlegrounds
+	Developed By: L, CiBeR, neves768, 1BOY
+
+]]--
+
+-- // For Setup // --
+gameplayVariables = {}
+
+outputDebugString("[MTA:BG] cfgBaseDefines loaded")
