3c434e6a10c0afde22b72a5a1e92fb1f18f7d2b1
ciber96 <keevgross@gmail.com>
2017-08-21 15:28:08 -0300

    Added serial register checking function

--- battlegrounds/accounting/account_system.lua
+++ battlegrounds/accounting/account_system.lua
@@ -23,3 +23,19 @@ function mtabg_mysql_init()
 
 end
 addEventHandler("onResourceStart", resourceRoot, mtabg_mysql_init)
+
+-- Returns 0 serial invalid/non existing | 1 serial valid/existing | 2 for error
+function checkAccount(serial)
+    if serial then
+        local query = "SELECT id FROM accounts WHERE serial="..serial..";"
+        local rQuery = dbQuery(mysql_link, query)
+        local rPoll = dbPoll(rQuery, -1)
+        if #rPoll > 0 then
+            return 1
+        else
+            return 0
+        end
+    else
+        return 2
+    end
+end
