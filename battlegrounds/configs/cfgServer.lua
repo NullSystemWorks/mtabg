5320c4b50c8b61ceab0e9491c4b73a35df408f66
ciber96 <keevgross@gmail.com>
2017-08-21 14:37:06 -0300

    Revert "MySQL Implmentation DB Init"
    This reverts commit 988e38e173f7c2b60cd9b8563fc2331088b4f569.

--- battlegrounds/configs/cfgServer.lua
+++ battlegrounds/configs/cfgServer.lua
@@ -18,11 +18,3 @@ gameplayVariables['afktime'] = 300 -- Max AFK time before auto-kill | DEFAULT: 3
 
 --[ Enviorment Settings ]--
 gameplayVariables['radiationrate'] = 1 -- Multiplier for damage taken by radiation | DEFAULT: 1
-
-
---[ Database Settings ]--
-gameplayVariables['database'] = 0 -- Define DB type to use | 0 = MySQL | 1 = Default MTA(Not Recommended| Deprecated)
-gameplayVariables['db_host'] = "127.0.0.1" -- Define our MySQL Host | DEFAULT: 127.0.0.1
-gameplayVariables['db_user'] = '' -- Define our MySQL User
-gameplayVariables['db_port'] = 3306 -- Define our MySQL Port | DEFAULT: 3306
-gameplayVariables['db_name'] = 'mtabg' -- Define our MySQL Database name | DEFAULT: mtabg
