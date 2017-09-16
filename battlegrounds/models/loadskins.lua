cbab307a56825ad608efab3147aa6e91de73d7ff
ciber96 <keevgross@gmail.com>
2017-09-15 21:58:39 -0300

    Rewrite model loading

--- battlegrounds/models/loadskins.lua
+++ battlegrounds/models/loadskins.lua
@@ -6,86 +6,44 @@
 
 ]]--
 
+-- Define models
+local models = {
+	[2647] = "cola_can",
+	[1578] = "bandage",
+	[2709] = "painkillers",
+	[2891] = "first_aid_kit",
+	[1650] = "jerrycan",
+	[2057] = "9x18cartridge",
+	[2041] = "9x19cartridge",
+	[2358] = "12gauge",
+	[2036] = "308winchester",
+	[1271] = "545x39cartridge",
+	[2035] = "556x45cartridge",
+	[2034] = "762x51cartridge",
+	[2358] = "762x54cartridge",
+	[3013] = "1143x23cartridge",
+	[2033] = "1866slug",
+	[3026] = "backpack_pouch",
+	[1644] = "backpack_alice",
+	[1239] = "backpack_survival"
+}
 
+-- Define model tables
+modelTXD = {}
+modelDFF = {}
 
-itemTXD = engineLoadTXD("models/items/txd/cola_can.txd")
-engineImportTXD(itemTXD, 2647)
-itemDFF = engineLoadDFF("models/items/dff/cola_can.dff", 2647)
-engineReplaceModel(itemDFF, 2647)
-itemTXD = engineLoadTXD("models/items/txd/bandage.txd")
-engineImportTXD(itemTXD, 1578)
-itemDFF = engineLoadDFF("models/items/dff/bandage.dff", 1578)
-engineReplaceModel(itemDFF, 1578)
-itemTXD = engineLoadTXD("models/items/txd/painkillers.txd")
-engineImportTXD(itemTXD, 2709)
-itemDFF = engineLoadDFF("models/items/dff/painkillers.dff", 2709)
-engineReplaceModel(itemDFF, 2709)
-itemTXD = engineLoadTXD("models/items/txd/first_aid_kit.txd")
-engineImportTXD(itemTXD, 2891)
-itemDFF = engineLoadDFF("models/items/dff/first_aid_kit.dff", 2891)
-engineReplaceModel(itemDFF, 2891)
-itemTXD = engineLoadTXD("models/items/txd/jerrycan.txd")
-engineImportTXD(itemTXD, 1650)
-itemDFF = engineLoadDFF("models/items/dff/jerrycan.dff", 1650)
-engineReplaceModel(itemDFF, 1650)
+function loadModels()
+	for i, v in pairs(models) do
+		-- Load TXD
+		modelTXD[i] = engineLoadTXD("models/items/txd/"..v..".txd")
+		engineImportTXD(modelTXD[i], i)
+		-- Load DFF
+		modelDFF[i] = engineLoadDFF("models/items/dff/"..v..".dff", i)
+		engineReplaceModel(modelDFF[i], i)
+	end
+end
 
-
-itemTXD = engineLoadTXD("models/items/txd/9x18cartridge.txd")
-engineImportTXD(itemTXD, 2057)
-itemDFF = engineLoadDFF("models/items/dff/9x18cartridge.dff", 2057)
-engineReplaceModel(itemDFF, 2057)
-itemTXD = engineLoadTXD("models/items/txd/9x19cartridge.txd")
-engineImportTXD(itemTXD, 2041)
-itemDFF = engineLoadDFF("models/items/dff/9x19cartridge.dff", 2041)
-engineReplaceModel(itemDFF, 2041)
-itemTXD = engineLoadTXD("models/items/txd/12gauge.txd")
-engineImportTXD(itemTXD, 2358)
-itemDFF = engineLoadDFF("models/items/dff/12gauge.dff", 2358)
-engineReplaceModel(itemDFF, 2358)
-itemTXD = engineLoadTXD("models/items/txd/308winchester.txd")
-engineImportTXD(itemTXD, 2036)
-itemDFF = engineLoadDFF("models/items/dff/308winchester.dff", 2036)
-engineReplaceModel(itemDFF, 2036)
-itemTXD = engineLoadTXD("models/items/txd/545x39cartridge.txd")
-engineImportTXD(itemTXD, 1271)
-itemDFF = engineLoadDFF("models/items/dff/545x39cartridge.dff", 1271)
-engineReplaceModel(itemDFF, 1271)
-itemTXD = engineLoadTXD("models/items/txd/556x45cartridge.txd")
-engineImportTXD(itemTXD, 2035)
-itemDFF = engineLoadDFF("models/items/dff/556x45cartridge.dff", 2035)
-engineReplaceModel(itemDFF, 2035)
-itemTXD = engineLoadTXD("models/items/txd/762x51cartridge.txd")
-engineImportTXD(itemTXD, 2034)
-itemDFF = engineLoadDFF("models/items/dff/762x51cartridge.dff", 2034)
-engineReplaceModel(itemDFF, 2034)
-itemTXD = engineLoadTXD("models/items/txd/762x54cartridge.txd")
-engineImportTXD(itemTXD, 2358)
-itemDFF = engineLoadDFF("models/items/dff/762x54cartridge.dff", 2358)
-engineReplaceModel(itemDFF, 2358)
-itemTXD = engineLoadTXD("models/items/txd/1143x23cartridge.txd")
-engineImportTXD(itemTXD, 3013)
-itemDFF = engineLoadDFF("models/items/dff/1143x23cartridge.dff", 3013)
-engineReplaceModel(itemDFF, 3013)
-itemTXD = engineLoadTXD("models/items/txd/1866slug.txd")
-engineImportTXD(itemTXD, 2033)
-itemDFF = engineLoadDFF("models/items/dff/1866slug.dff", 2033)
-engineReplaceModel(itemDFF, 2033)
-
-
-itemTXD = engineLoadTXD("models/items/txd/backpack_pouch.txd")
-engineImportTXD(itemTXD, 3026)
-itemDFF = engineLoadDFF("models/items/dff/backpack_pouch.dff", 3026)
-engineReplaceModel(itemDFF, 3026)
-itemTXD = engineLoadTXD("models/items/txd/backpack_alice.txd")
-engineImportTXD(itemTXD, 1644)
-itemDFF = engineLoadDFF("models/items/dff/backpack_alice.dff", 1644)
-engineReplaceModel(itemDFF, 1644)
-itemTXD = engineLoadTXD("models/items/txd/backpack_survival.txd")
-engineImportTXD(itemTXD, 1239)
-itemDFF = engineLoadDFF("models/items/dff/backpack_survival.dff", 1239)
-engineReplaceModel(itemDFF, 1239)
-
-
+--[[
 weaponSkinTable = {
 {"m1911",2,"M1911"},
 {"revolver",2,"Revolver"},
@@ -134,7 +92,7 @@ function loadTheSkins()
 		weaponDFF[i] = engineLoadDFF("models/weapons/dff/"..skin[1]..".dff", 0)
 	end
 end
---addEventHandler("onClientResourceStart",getResourceRootElement(getThisResource()),loadTheSkins)
-
+addEventHandler("onClientResourceStart",getResourceRootElement(getThisResource()),loadTheSkins)
+]]--
 
 
