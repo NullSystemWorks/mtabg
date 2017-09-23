0406dd7c49447229ec0c84e0b93c6cd00a7eced9
expert975 <rodrigocomputadores125@hotmail.com>
2017-09-23 12:15:39 -0300

    Cleanup 8c1be5383

--- battlegrounds/handlers/server/permissions.lua
+++ battlegrounds/handlers/server/permissions.lua
@@ -3,26 +3,16 @@
 				MTA:BG
 			MTA Battlegrounds
 	Developed By: Null System Works (L, CiBeR, neves768, 1BOY & expert975)
-	
+
 ]]--
 
-
-function preventCertainCommands(cmd)
-	if cmd == "say" then
-		for i, players in ipairs(getElementsByType("player")) do
-			if not getElementData(players,"inLobby") then
-				cancelEvent()
-				return
-			end
-		end
-	end
-	if cmd == "teamsay" then
-		cancelEvent()
-		return
-	end
-	if cmd == "register" then -- login will be enabled for now, until we have a proper admin panel that does not MTA's login system
-		cancelEvent()
-		return
+local function restrictCommands(cmd)
+	if (cmd == "say" and not getElementData(source,"inLobby"))
+		or cmd == "teamsay"
+		or cmd == "showchat"
+		or cmd == "register"
+		or cmd == "me" then
+			cancelEvent()
 	end
 end
-addEventHandler("onPlayerCommand",root,preventCertainCommands)
+addEventHandler("onPlayerCommand",root,restrictCommands)
--- battlegrounds/world/sWorld.lua
+++
@@ -1,6 +0,0 @@
-function preventChatSay(cmd)
-	if cmd == "say" or cmd == "teamsay" or cmd == "showchat" or cmd == "me" then
-		cancelEvent()
-	end
-end
-addEventHandler("onPlayerCommand",root,preventChatSay)
