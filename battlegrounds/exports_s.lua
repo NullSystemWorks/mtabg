0931166505620982957223fa36907aee44731f58
ciber96 <keevgross@gmail.com>
2017-09-28 14:03:06 -0300

    Added 2 export functions: setAFKTime & setRadiationRate

--- battlegrounds/exports_s.lua
+++ battlegrounds/exports_s.lua
@@ -12,4 +12,33 @@ function isGameRunning()
     else
         return false
     end
+	return false
+end
+
+function setAFKTime(sec)
+	if sec and sec > 4 then
+		gameplayVariables['afktime'] = sec
+		for i, v in ipairs(getElementsByType("player"))do
+			outputChatBox("[CONFIG] AFK TIme changed to: "..tostring(sec),v,0,255,0,true)
+		end
+		exports.battlegrounds:saveLog("[CONFIG] AFK Time changed to: "..tostring(sec).."\n", "game")
+		return true
+	else
+		return false
+	end
+	return false
+end
+
+function setRadiationRate(rate)
+	if rate then
+		gameplayVariables['radiationrate'] = rate
+		for i, v in ipairs(getElementsByType("player"))do
+			outputChatBox("[CONFIG] Radiation Rate changed to: "..tostring(rate),v,0,255,0,true)
+		end
+		exports.battlegrounds:saveLog("[CONFIG] Radiation Rate changed to: "..tostring(rate).."\n", "game")
+		return true
+	else
+		return false
+	end
+	return false
 end
