814e7d952a4f9701da75c707b74a8b95227a21e4
expert975 <rodrigocomputadores125@hotmail.com>
2017-10-14 15:52:19 -0300

    Add guiComboBoxAdjustHeight() to libraries

---
+++ battlegrounds/lib/graphics2D/cMisc.lua
@@ -0,0 +1,5 @@
+function guiComboBoxAdjustHeight ( combobox, itemcount )
+    if getElementType ( combobox ) ~= "gui-combobox" or type ( itemcount ) ~= "number" then error ( "Invalid arguments @ 'guiComboBoxAdjustHeight'", 2 ) end
+    local width = guiGetSize ( combobox, false )
+    return guiSetSize ( combobox, width, ( itemcount * 20 ) + 20, false )
+end
