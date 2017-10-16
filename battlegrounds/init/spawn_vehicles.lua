5a7e066ab6d9a22db0e0f24e784e3e8368eeade6
expert975 <rodrigocomputadores125@hotmail.com>
2017-10-16 17:51:44 -0200

    Move getElementSpeed function to math lib

--- battlegrounds/init/spawn_vehicles.lua
+++ battlegrounds/init/spawn_vehicles.lua
@@ -55,21 +55,6 @@ function checkFuelOfVehicle()
 	end
 end
 
---Useful Function
-function getElementSpeed(theElement, unit)
-    -- Check arguments for errors
-    assert(isElement(theElement), "Bad argument 1 @ getElementSpeed (element expected, got " .. type(theElement) .. ")")
-    local elementType = getElementType(theElement)
-    assert(elementType == "player" or elementType == "ped" or elementType == "object" or elementType == "vehicle" or elementType == "projectile", "Invalid element type @ getElementSpeed (player/ped/object/vehicle/projectile expected, got " .. elementType .. ")")
-    assert((unit == nil or type(unit) == "string" or type(unit) == "number") and (unit == nil or (tonumber(unit) and (tonumber(unit) == 0 or tonumber(unit) == 1 or tonumber(unit) == 2)) or unit == "m/s" or unit == "km/h" or unit == "mph"), "Bad argument 2 @ getElementSpeed (invalid speed unit)")
-    -- Default to m/s if no unit specified and 'ignore' argument type if the string contains a number
-    unit = unit == nil and 0 or ((not tonumber(unit)) and unit or tonumber(unit))
-    -- Setup our multiplier to convert the velocity to the specified unit
-    local mult = (unit == 0 or unit == "m/s") and 50 or ((unit == 1 or unit == "km/h") and 180 or 111.84681456)
-    -- Return the speed by calculating the length of the velocity vector, after converting the velocity to the specified unit
-    return (Vector3(getElementVelocity(theElement)) * mult).length
-end
-
 function setVehicleFuelConsumption(veh,player,seat)
 	if veh then
 		if seat == 0 then
--- battlegrounds/lib/math.lua
+++ battlegrounds/lib/math.lua
@@ -119,3 +119,17 @@ end
 function mapValues(x, in_min, in_max, out_min, out_max) --rescales values
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 end
+
+function getElementSpeed(theElement, unit)
+	-- Check arguments for errors
+	assert(isElement(theElement), "Bad argument 1 @ getElementSpeed (element expected, got " .. type(theElement) .. ")")
+	local elementType = getElementType(theElement)
+	assert(elementType == "player" or elementType == "ped" or elementType == "object" or elementType == "vehicle" or elementType == "projectile", "Invalid element type @ getElementSpeed (player/ped/object/vehicle/projectile expected, got " .. elementType .. ")")
+	assert((unit == nil or type(unit) == "string" or type(unit) == "number") and (unit == nil or (tonumber(unit) and (tonumber(unit) == 0 or tonumber(unit) == 1 or tonumber(unit) == 2)) or unit == "m/s" or unit == "km/h" or unit == "mph"), "Bad argument 2 @ getElementSpeed (invalid speed unit)")
+	-- Default to m/s if no unit specified and 'ignore' argument type if the string contains a number
+	unit = unit == nil and 0 or ((not tonumber(unit)) and unit or tonumber(unit))
+	-- Setup our multiplier to convert the velocity to the specified unit
+	local mult = (unit == 0 or unit == "m/s") and 50 or ((unit == 1 or unit == "km/h") and 180 or 111.84681456)
+	-- Return the speed by calculating the length of the velocity vector, after converting the velocity to the specified unit
+	return (Vector3(getElementVelocity(theElement)) * mult).length
+end
